341aaaf715478a1b8a20cd6c6bc18b76c45c997a
kinstel solutions <kinstelsolutions@gmail.com>
2025-12-14 09:24:32 +0000

    centre the disclamer

--- src/components/sections/disclaimer.tsx
+++ src/components/sections/disclaimer.tsx
@@ -23,7 +23,7 @@ const DisclaimerDialog = () => {
                 <AlertDialogHeader className="text-center">
                     <AlertDialogTitle className="font-headline text-2xl text-primary">Disclaimer</AlertDialogTitle>
                     <AlertDialogDescription asChild>
-                        <div className="text-muted-foreground text-sm max-h-[60vh] overflow-y-auto pr-4 text-left">
+                        <div className="text-muted-foreground text-sm max-h-[60vh] overflow-y-auto pr-4 text-center">
                         <p className="mt-4">
                             The rules of the Indian Bar Council prohibit law firms from advertising and soliciting work through communication in the public domain. This website is meant solely for the purpose of information and not for the purpose of advertising. Singh Associates does not intend to solicit clients through this website. We do not take responsibility for decisions taken by the reader based solely on the information provided in the website. By continuing to use this website, the visitor acknowledges that the information provided in the website (a) does not amount to advertising or solicitation and (b) is meant only for his/her understanding about our activities and who we are.
                         </p>
